7163220b8df7883bc8fc55feec079897db6e5d4f
Eze Ransom <148370144+Ransom070400@users.noreply.github.com>
2024-10-11 00:52:04 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
 	return (
 		<div className={styles.container}>
-			<ConnectSUIWallet theme='dark' />
+			<ConnectWallet theme='dark' />
 		</div>
 	);
 }
